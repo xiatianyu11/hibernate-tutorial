25d342f8ee9cfe1342aeed33bacf2fb26996c0cd
Xia Tianyu <41894660@qq.com>
2017-09-01 23:03:57 +0800

    sql optimization

--- FirstHibernateProj/src/com/rain/cache/dto/User.java
+++ FirstHibernateProj/src/com/rain/cache/dto/User.java
@@ -5,19 +5,23 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Version;
 
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 @Entity
-@Cacheable
-@Cache(usage=CacheConcurrencyStrategy.READ_ONLY)
+//@Cacheable
+//@Cache(usage=CacheConcurrencyStrategy.READ_ONLY)
 public class User {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int userId;
 	
 	private String userName;
+	
+	@Version
+	private Integer version;
 
 	public int getUserId() {
 		return userId;
@@ -33,6 +37,14 @@ public class User {
 
 	public void setUserName(String userName) {
 		this.userName = userName;
+	}
+
+	public Integer getVersion() {
+		return version;
+	}
+
+	public void setVersion(Integer version) {
+		this.version = version;
 	}
 	
 	
--- FirstHibernateProj/src/com/rain/cache/hibernate/HibernateTest.java
+++ FirstHibernateProj/src/com/rain/cache/hibernate/HibernateTest.java
@@ -1,7 +1,9 @@
 package com.rain.cache.hibernate;
 
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 import com.rain.cache.dto.User;
@@ -10,30 +12,44 @@ public class HibernateTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		User user = new User();
-		user.setUserName("xxx");
-		SessionFactory factory = new Configuration().configure().buildSessionFactory();
-		Session session = factory.openSession();
-		session.beginTransaction();
-		session.save(user);
-		session.getTransaction().commit();
-		session.close();
+
+		try {
+			SessionFactory factory = new Configuration().configure().buildSessionFactory();
+			Session session1 = factory.openSession();
+			Session session2 = factory.openSession();
+			User user1 = (User) session1.get(User.class, 1);
+			User user2 = (User) session2.get(User.class, 1);
+			System.out.println(user1.getVersion());
+			System.out.println(user2.getVersion());
+			
+			
+			
+			Transaction tx1 = session1.beginTransaction();
+			user1.setUserName("bbb");
+			session1.update(user1);
+			tx1.commit();
+			session1.close();
+			System.out.println(user1.getVersion() + "---");
+			System.out.println(user2.getVersion());
+			
+			
+			Transaction tx2 = session2.beginTransaction();
+			user2.setUserName("dddd");
+			session2.update(user2);
+			//session2.delete(user2);
+			tx2.commit();
+			
+			System.out.println(user1.getVersion());
+			System.out.println(user2.getVersion());
+			
+			
+			
+			session2.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		
-		session = factory.openSession();
-		session.beginTransaction();
-		User user1 = (User) session.get(User.class, 1);
-		User user2 = (User) session.get(User.class, 1);
-		System.out.println(user2.getUserName());
-		session.getTransaction().commit();
-		session.close();
-		
-		session = factory.openSession();
-		session.beginTransaction();
-		User user3 = (User) session.get(User.class, 1);
-		System.out.println(user2.getUserName());
-		session.getTransaction().commit();
-		session.close();
 		
 		
 	}
